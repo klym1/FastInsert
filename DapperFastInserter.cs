abe4c59c6d87317ea7f50a2206eab4965107bafa
Mykola Klymyuk <mykola.klymyuk@gmail.com>
2019-07-31 23:46:49 +0300

    Extracted everything to an extensions method, tidied up. Starting to add support for some special types (DateTime, Guid) which won't get imported properly with the default csv serialization

---
+++ DapperFastInserter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using Dapper;
+
+namespace DapperFastInsert
+{
+    public static class DapperFastInserter
+    {
+        public static async Task<int> FastInsertAsync<T>(this IDbConnection connection, IEnumerable<T> list, string tableName)
+        {
+            var fileName = "temp.csv";
+            
+            var tableColumns = GetTableColumns(connection, tableName, connection.Database);
+            var columnIndexes = GetColumnIndexes(tableColumns);
+            await WriteToCsvFileAsync(list, columnIndexes, fileName);
+
+            var query = BuildQuery(tableName, fileName);
+            return await connection.ExecuteAsync(query);
+        }
+
+        private static IDictionary<string, int> GetColumnIndexes(IEnumerable<string> columns)
+        {
+            return columns
+                .Select((it, index) => (it, index))
+                .ToDictionary(it => it.it, it => it.index, StringComparer.OrdinalIgnoreCase);
+        }
+
+        private static string BuildQuery(string tableName, string tempFilePath)
+        {
+            var lineEnding = Environment.NewLine;
+
+            return $@"LOAD DATA LOCAL INFILE '{tempFilePath}' 
+                   INTO TABLE `{tableName}` 
+                   FIELDS TERMINATED BY ';' 
+                   LINES TERMINATED BY '{lineEnding}' 
+                   IGNORE 1 LINES";
+        }
+
+        private static Task WriteToCsvFileAsync<T>(IEnumerable<T> list, IDictionary<string, int> dict, string fileName)
+        {
+            using var fileStream = new FileStream(fileName, FileMode.Create);
+            using TextWriter textWriter = new StreamWriter(fileStream);
+            using var writer = new CsvWriter(textWriter);
+            writer.Configuration.HasHeaderRecord = true;
+
+            var opt1 = writer.Configuration.TypeConverterOptionsCache.GetOptions<DateTime>();
+            opt1.DateTimeStyle = DateTimeStyles.AssumeUniversal;
+            opt1.Formats = new[] { "O" };
+
+            //var opt2 = writer.Configuration.TypeConverterOptionsCache.GetOptions<Guid>();
+            //opt2.Formats = new[] { "N" };
+            
+            writer.Configuration.TypeConverterCache.AddConverter(typeof(Guid), new GuidFormatter());
+
+            var map = writer.Configuration.AutoMap<T>();
+            SortColumns(dict, map);
+
+            writer.WriteRecords(list);
+            return Task.FromResult(0);
+        }
+
+        private static void SortColumns(IDictionary<string, int> dict, ClassMap map)
+        {
+            foreach (var memberMap in map.MemberMaps)
+            {
+                var index = dict[memberMap.Data.Names[0]];
+                memberMap.Index(index);
+            }
+        }
+
+        private static IEnumerable<string> GetTableColumns(IDbConnection connection, string tableName, string dbName)
+        {
+            return connection.Query<string>($@"SELECT c.column_name
+            FROM INFORMATION_SCHEMA.COLUMNS c
+            WHERE c.table_name = '{tableName}'
+                -- AND c.table_schema = '{dbName}'");
+        }
+    }
+
+    public class GuidFormatter : DefaultTypeConverter
+    {
+        public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            var bytes = ((Guid)value).ToByteArray();
+            var str = Encoding.UTF8.GetString(bytes);
+            return str;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using CsvHelper;
-using CsvHelper.Configuration;
-using Dapper;
 using MySql.Data.MySqlClient;
-
 
 namespace DapperFastInsert
 {
@@ -18,7 +10,7 @@ namespace DapperFastInsert
     {
         static async Task Main()
         {
-            var connBuilder = new MySqlConnectionStringBuilder()
+            var connBuilder = new MySqlConnectionStringBuilder
             {
                 AllowLoadLocalInfile = true,
                 Database = "fastinsert",
@@ -26,92 +18,33 @@ namespace DapperFastInsert
                 Password = "root"
             };
 
+            var tableName = "test";
+
             var conn = connBuilder.ToString();
 
             var connection = new MySqlConnection(conn);
 
-            var list = Enumerable.Range(1, 10)
+            var list = Enumerable.Range(1, 100000)
                 .Select(it =>
                     new Table
                     {
                         Int = it,
-                        Text = "text" + it
+                        Text = "text" + it,
+                        DateCol = DateTime.UtcNow.AddHours(it),
+                        Guid = Guid.NewGuid()
                     });
-
-            var fileName = "temp.csv";
-
-            var tableColumns = GetTableColumns(connection, "test");
-
-            var columnIndexes = GetColumnIndexes(tableColumns);
-            await WriteToCsvFileAsync(list, columnIndexes, fileName);
-
-            var query = BuildQuery("test", fileName);
-
+            
             var sw = Stopwatch.StartNew();
-            await connection.ExecuteAsync(query);
+            await connection.FastInsertAsync(list, tableName);
             sw.Stop();
             Console.WriteLine($"Inserted in {sw.ElapsedMilliseconds} ms");
-        }
-
-        private static IDictionary<string, int> GetColumnIndexes(IEnumerable<string> columns)
-        {
-            return columns
-                .Select((it, index) => (it, index))
-                .ToDictionary(it => it.it, it => it.index, StringComparer.OrdinalIgnoreCase);
-        }
-
-        private static string BuildQuery(string tableName, string tempFilePath)
-        {
-            var lineEnding = Environment.NewLine;
-
-            return $@"LOAD DATA LOCAL INFILE '{tempFilePath}' 
-                   INTO TABLE `{tableName}` 
-                   FIELDS TERMINATED BY ';' 
-                   LINES TERMINATED BY '{lineEnding}' 
-                   IGNORE 1 LINES";
-
-        }
-
-        private static Task WriteToCsvFileAsync(IEnumerable<Table> list, IDictionary<string, int> dict, string fileName)
-        {
-            using var fileStream = new FileStream(fileName, FileMode.Create);
-            using TextWriter textWriter = new StreamWriter(fileStream);
-            using var writer = new CsvWriter(textWriter);
-
-            writer.Configuration.HasHeaderRecord = true;
-            writer.Configuration.DynamicPropertySort = new ColumnComparer();
-
-            var map = writer.Configuration.AutoMap<Table>();
-
-            foreach (var memberMap in map.MemberMaps)
-            {
-                var index = dict[memberMap.Data.Names[0]];
-                memberMap.Index(index);
-            }
-            
-            writer.WriteRecords(list);
-            return Task.FromResult(0);
-        }
-
-        private static IEnumerable<string> GetTableColumns(IDbConnection connection, string tableName)
-        {
-            return connection.Query<string>($@"SELECT c.column_name
-            FROM INFORMATION_SCHEMA.COLUMNS c
-            WHERE c.table_name = '{tableName}'
-                -- AND c.table_schema = 'db_name'");
-        }
-    }
-
-    public class ColumnComparer : IComparer<string>
-    {
-        public int Compare(string x, string y)
-        {
-            return string.Compare(x, y, StringComparison.InvariantCulture) + 1;
         }
     }
 
     public class Table
     {
+        public Guid Guid { get; set; }
+        public DateTime DateCol { get; set; }
         public int Int { get; set; }
         public string Text { get; set; }
     }
