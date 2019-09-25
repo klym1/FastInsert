774a8a09e0f5931df8e5e5ce3adc03617ab6272d
Mykola Klymyuk <mykola.klymyuk@gmail.com>
2019-09-26 00:20:43 +0300

    Added test suite, written first integration test.

---
+++ src/FastInsert.Tests/MySqlGuidTypeHandler.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Data;
+using Dapper;
+
+namespace FastInsert.Tests
+{
+    //taken from here: https://github.com/StackExchange/Dapper/issues/447#issuecomment-277589028
+    public class MySqlGuidTypeHandler : SqlMapper.TypeHandler<Guid>
+    {
+        public override void SetValue(IDbDataParameter parameter, Guid value)
+        {
+            parameter.Value = FlipEndian(value.ToByteArray());
+        }
+
+        public override Guid Parse(object value)
+        {
+            return new Guid(FlipEndian((byte[])value));
+        }
+
+        internal static byte[] FlipEndian(byte[] oldBytes)
+        {
+            var newBytes = new byte[16];
+            for (var i = 8; i < 16; i++)
+                newBytes[i] = oldBytes[i];
+
+            newBytes[3] = oldBytes[0];
+            newBytes[2] = oldBytes[1];
+            newBytes[1] = oldBytes[2];
+            newBytes[0] = oldBytes[3];
+            newBytes[5] = oldBytes[4];
+            newBytes[4] = oldBytes[5];
+            newBytes[6] = oldBytes[7];
+            newBytes[7] = oldBytes[6];
+
+            return newBytes;
+        }
+    }
+}
---
+++ src/FastInsert.Tests/SimpleTests.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using Xunit;
+
+namespace FastInsert.Tests
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public async Task GeneratedDataIsCorrectlyInserted()
+        {
+            SqlMapper.RemoveTypeMap(typeof(Guid));
+            SqlMapper.RemoveTypeMap(typeof(Guid?));
+            SqlMapper.AddTypeHandler(new MySqlGuidTypeHandler());
+
+            var connBuilder = new MySqlConnectionStringBuilder
+            {
+                AllowLoadLocalInfile = true,
+                AllowUserVariables = true,
+                Database = "tests",
+                UserID = "root",
+                Password = "root"
+            };
+
+            var conn = connBuilder.ToString();
+            var connection = new MySqlConnection(conn);
+
+            var list = GenerateData().ToList();
+
+            await connection.ExecuteAsync("drop table if exists test");
+            await connection.ExecuteAsync(@"
+                CREATE TABLE IF NOT EXISTS `test` (
+                  `guid` binary(16) NOT NULL,
+                  `dateCol` datetime(3) NOT NULL,
+                  `int` int NOT NULL,
+                  `text` text NOT NULL
+                  );  ");
+
+            await connection.FastInsertAsync(list, "test");
+
+            var actualData = (await connection.QueryAsync<Table>("select * from test")).ToList();
+
+            Assert.Equal(list[0].DateCol, actualData[0].DateCol, TimeSpan.FromMilliseconds(1));
+            Assert.Equal(list[0].Guid, actualData[0].Guid);
+            Assert.Equal(list[0].Int, actualData[0].Int);
+            Assert.Equal(list[0].Text, actualData[0].Text);
+        }
+
+        private static IEnumerable<Table> GenerateData()
+        {
+            return Enumerable.Range(1, 1)
+                .Select(it =>
+                    new Table
+                    {
+                        Int = it,
+                        Text = "text" + it,
+                        DateCol = DateTime.UtcNow.AddHours(it),
+                        Guid = Guid.NewGuid()
+                    });
+        }
+    }
+
+    public class Table
+    {
+        public Guid Guid { get; set; }
+        public DateTime DateCol { get; set; }
+        public int Int { get; set; }
+        public string Text { get; set; }
+    }
+}
