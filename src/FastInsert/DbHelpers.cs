ede818ca137862ddbaa69abf7d1a366f6f5648f9
Mykola Klymyuk <mykola.klymyuk@gmail.com>
2019-11-10 15:35:05 +0200

    ...

---
+++ src/FastInsert/DbHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+static internal class DbHelpers
+{
+    public static IEnumerable<string> GetTableColumns(IDbConnection connection, string tableName, string dbName)
+    {
+        using var command = connection.CreateCommand();
+        command.CommandText = @"SELECT c.column_name
+            FROM INFORMATION_SCHEMA.COLUMNS c
+            WHERE c.table_name = @tableName
+                 AND c.table_schema = @schema";
+
+        var param1 = command.CreateParameter();
+        param1.ParameterName = "tableName";
+        param1.Value = tableName;
+            
+        var param2 = command.CreateParameter();
+        param2.ParameterName = "schema";
+        param2.Value = dbName;
+
+        command.Parameters.Add(param1);
+        command.Parameters.Add(param2);
+
+        using var reader = command.ExecuteReader();
+
+        while (!reader.IsClosed && reader.Read())
+        {
+            var str = reader.GetString(0);
+            yield return str;
+        }
+    }
+}
--- src/FastInsert/FastInserter.cs
+++ src/FastInsert/FastInserter.cs
@@ -36,7 +36,7 @@ namespace FastInsert
 
             var tableName = config.TableNameResolver.GetTableName();
 
-            var tableColumns = GetTableColumns(connection, tableName, connection.Database);
+            var tableColumns = DbHelpers.GetTableColumns(connection, tableName, connection.Database);
             var classConfig = GetConfiguration<T>();
             var classFields = GetClassFields(classConfig);
             var tableDef = TableDefinitionFactory.BuildTableDefinition(classFields);
@@ -121,34 +121,6 @@ namespace FastInsert
             writer.Configuration.RegisterClassMap(classMap);
 
             writer.WriteRecords(list);
-        }
-        
-        private static IEnumerable<string> GetTableColumns(IDbConnection connection, string tableName, string dbName)
-        {
-            using var command = connection.CreateCommand();
-            command.CommandText = @"SELECT c.column_name
-            FROM INFORMATION_SCHEMA.COLUMNS c
-            WHERE c.table_name = @tableName
-                 AND c.table_schema = @schema";
-
-            var param1 = command.CreateParameter();
-            param1.ParameterName = "tableName";
-            param1.Value = tableName;
-            
-            var param2 = command.CreateParameter();
-            param2.ParameterName = "schema";
-            param2.Value = dbName;
-
-            command.Parameters.Add(param1);
-            command.Parameters.Add(param2);
-
-            using var reader = command.ExecuteReader();
-
-            while (!reader.IsClosed && reader.Read())
-            {
-                var str = reader.GetString(0);
-                yield return str;
-            }
         }
     }
 
